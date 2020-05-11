e2f1172e50f6274f117a963881ca1b116a3ec6ca
Johannes Kinast <johannes.kinast@gmail.com>
2020-05-11 10:48:09 +0200

    Renamed to Gallers Block Lightbox, add Namespace

--- gallery-block-lightbox.php
+++ gallery-block-lightbox.php
@@ -1,26 +1,25 @@
 <?php
 /**
- * Plugin Name:  Gutenberg Gallery Lightbox
+ * Plugin Name:  Gallery Block Lightbox
  * Plugin URI:
- * Description:  Adds a Lightbox to the Gutenberg Gallery Block.
+ * Description:  Adds a Lightbox to the Block Editor (Gutenberg) Gallery Block.
  * Author:       Johannes Kinast <johannes@travel-dealz.de>
  * Author URI:   https://go-around.de
  * Version:     1.1.0
  */
+namespace Gallery_Block_Lightbox;
 
-
-
-function gutenberg_gallery_lightbox_register () {
+function register_assets() {
 	wp_register_script( 'gutenberg-gallery-lightbox', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.js', [], '1.11.1', true );
 	wp_add_inline_script( 'gutenberg-gallery-lightbox', 'var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.textContent}};baguetteBox.run(".wp-block-gallery",options);' );
 	wp_register_style( 'gutenberg-gallery-lightbox-css', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.css', [], '1.11.1' );
 }
-add_action( 'wp_enqueue_scripts', 'gutenberg_gallery_lightbox_register' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets' );
 
-function gutenberg_gallery_lightbox_enqueue() {
+function enqueue_assets() {
 	if ( has_block( 'gallery' ) ) {
 		wp_enqueue_script( 'gutenberg-gallery-lightbox' );
 		wp_enqueue_style( 'gutenberg-gallery-lightbox-css' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'gutenberg_gallery_lightbox_enqueue' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
