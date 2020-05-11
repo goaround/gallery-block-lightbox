75eaf396e43e4018014c05ac3e456a877f1c1d96
Johannes Kinast <johannes.kinast@gmail.com>
2020-05-11 11:34:16 +0200

    Handler changed

--- gallery-block-lightbox.php
+++ gallery-block-lightbox.php
@@ -10,16 +10,16 @@
 namespace Gallery_Block_Lightbox;
 
 function register_assets() {
-	wp_register_script( 'gutenberg-gallery-lightbox', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.js', [], '1.11.1', true );
-	wp_add_inline_script( 'gutenberg-gallery-lightbox', 'var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.textContent}};baguetteBox.run(".wp-block-gallery",options);' );
-	wp_register_style( 'gutenberg-gallery-lightbox-css', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.css', [], '1.11.1' );
+	wp_register_script( 'gallery-block-lightbox', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.js', [], '1.11.1', true );
+	wp_add_inline_script( 'gallery-block-lightbox', 'var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.textContent}};baguetteBox.run(".wp-block-gallery",options);' );
+	wp_register_style( 'gallery-block-lightbox-css', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.css', [], '1.11.1' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets' );
 
 function enqueue_assets() {
 	if ( has_block( 'gallery' ) ) {
-		wp_enqueue_script( 'gutenberg-gallery-lightbox' );
-		wp_enqueue_style( 'gutenberg-gallery-lightbox-css' );
+		wp_enqueue_script( 'gallery-block-lightbox' );
+		wp_enqueue_style( 'gallery-block-lightbox-css' );
 	}
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
