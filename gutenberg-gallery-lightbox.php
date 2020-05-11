8832506a84b6f44a3237dcaee29606cc5710ea3a
Johannes Kinast <johannes.kinast@gmail.com>
2020-05-11 10:43:53 +0200

    add_action moved down

--- gutenberg-gallery-lightbox.php
+++ gutenberg-gallery-lightbox.php
@@ -9,17 +9,18 @@
  */
 
 
-add_action( 'wp_enqueue_scripts', 'gutenberg_gallery_lightbox_register' );
+
 function gutenberg_gallery_lightbox_register () {
 	wp_register_script( 'gutenberg-gallery-lightbox', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.js', [], '1.11.1', true );
 	wp_add_inline_script( 'gutenberg-gallery-lightbox', 'var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.textContent}};baguetteBox.run(".wp-block-gallery",options);' );
 	wp_register_style( 'gutenberg-gallery-lightbox-css', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.css', [], '1.11.1' );
 }
+add_action( 'wp_enqueue_scripts', 'gutenberg_gallery_lightbox_register' );
 
-add_action( 'wp_enqueue_scripts', 'gutenberg_gallery_lightbox_enqueue' );
 function gutenberg_gallery_lightbox_enqueue() {
 	if ( has_block( 'gallery' ) ) {
 		wp_enqueue_script( 'gutenberg-gallery-lightbox' );
 		wp_enqueue_style( 'gutenberg-gallery-lightbox-css' );
 	}
 }
+add_action( 'wp_enqueue_scripts', 'gutenberg_gallery_lightbox_enqueue' );
