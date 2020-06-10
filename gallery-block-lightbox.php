095c55a9d0f9bd9b4860c51762561daba998155a
Johannes Kinast <johannes.kinast@gmail.com>
2020-06-10 09:11:05 +0200

    Update gallery-block-lightbox.php

--- gallery-block-lightbox.php
+++ gallery-block-lightbox.php
@@ -5,7 +5,7 @@
  * Description:  Adds a Lightbox to the Block Editor (Gutenberg) Gallery Block.
  * Author:       Johannes Kinast <johannes@travel-dealz.de>
  * Author URI:   https://go-around.de
- * Version:     1.1.0
+ * Version:     1.1.1
  */
 namespace Gallery_Block_Lightbox;
 
