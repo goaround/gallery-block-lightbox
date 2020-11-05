0e9c3c769fb10a3fbf5f24a85d927accc3e58376
Johannes Kinast <johannes.kinast@gmail.com>
2020-11-05 16:00:33 +0100

    Support Image Block

--- gallery-block-lightbox.php
+++ gallery-block-lightbox.php
@@ -11,13 +11,13 @@ namespace Gallery_Block_Lightbox;
 
 function register_assets() {
 	wp_register_script( 'baguettebox', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.js', [], '1.11.1', true );
-	wp_add_inline_script( 'baguettebox', 'window.addEventListener("load", function() {var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.innerHTML}};baguetteBox.run(".wp-block-gallery",options);});' );
+	wp_add_inline_script( 'baguettebox', 'window.addEventListener("load", function() {var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.innerHTML}};baguetteBox.run(".wp-block-gallery",options);baguetteBox.run(".wp-block-image",options);});' );
 	wp_register_style( 'baguettebox-css', plugin_dir_url( __FILE__ ) . '/dist/baguetteBox.min.css', [], '1.11.1' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets' );
 
 function enqueue_assets() {
-	if ( has_block( 'gallery' ) ) {
+	if ( has_block( 'gallery' ) || has_block( 'image' ) ) {
 		wp_enqueue_script( 'baguettebox' );
 		wp_enqueue_style( 'baguettebox-css' );
 	}
