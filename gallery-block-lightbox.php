45d843d67c7c10920d70fe45f66a90a057746ddd
Johannes Kinast <johannes.kinast@gmail.com>
2021-01-11 12:41:07 +0100

    Add support for .svg Images

--- gallery-block-lightbox.php
+++ gallery-block-lightbox.php
@@ -11,7 +11,7 @@ namespace Gallery_Block_Lightbox;
 
 function register_assets() {
 	wp_register_script( 'baguettebox', plugin_dir_url( __FILE__ ) . 'dist/baguetteBox.min.js', [], '1.11.1', true );
-	wp_add_inline_script( 'baguettebox', 'window.addEventListener("load", function() {var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.innerHTML}};baguetteBox.run(".wp-block-gallery",options);baguetteBox.run(".wp-block-image",options);});' );
+	wp_add_inline_script( 'baguettebox', 'window.addEventListener("load", function() {var options={captions:function(t){var e=t.parentElement.getElementsByTagName("figcaption")[0];return!!e&&e.innerHTML},filter:/.+\.(gif|jpe?g|png|webp|svg)/i};baguetteBox.run(".wp-block-gallery",options);baguetteBox.run(".wp-block-image",options);});' );
 	wp_register_style( 'baguettebox-css', plugin_dir_url( __FILE__ ) . 'dist/baguetteBox.min.css', [], '1.11.1' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets' );
