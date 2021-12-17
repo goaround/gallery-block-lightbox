3ca1aa63f8136f72433b6b2e6202a6cb03f74676
Thomas Buchholz <733833@gmx.de>
2021-12-18 00:56:12 +0100

    feat: add filter to control enqueing of baguettebox assets

--- gallery-block-lightbox.php
+++ gallery-block-lightbox.php
@@ -14,21 +14,21 @@ function register_assets() {
 	wp_register_script( 'baguettebox', plugin_dir_url( __FILE__ ) . 'dist/baguetteBox.min.js', [], '1.11.1', true );
 
 	/**
-     * Filters the CSS selector of baguetteBox.js
-     *
-     * @since 1.10.0
-     *
-     * @param string  $value  The CSS selector to a gallery (or galleries) containing a tags
-     */
-	$baguettebox_selector = apply_filters('baguettebox_selector', '.wp-block-gallery,:not(.wp-block-gallery)>.wp-block-image,.wp-block-media-text__media,.gallery,.wp-block-coblocks-gallery-masonry,.wp-block-coblocks-gallery-stacked,.wp-block-coblocks-gallery-collage,.wp-block-coblocks-gallery-offset,.wp-block-coblocks-gallery-stacked' );
+	 * Filters the CSS selector of baguetteBox.js
+	 *
+	 * @since 1.10.0
+	 *
+	 * @param string  $value  The CSS selector to a gallery (or galleries) containing a tags
+	 */
+	$baguettebox_selector = apply_filters( 'baguettebox_selector', '.wp-block-gallery,:not(.wp-block-gallery)>.wp-block-image,.wp-block-media-text__media,.gallery,.wp-block-coblocks-gallery-masonry,.wp-block-coblocks-gallery-stacked,.wp-block-coblocks-gallery-collage,.wp-block-coblocks-gallery-offset,.wp-block-coblocks-gallery-stacked' );
 
 	/**
-     * Filters the image files filter of baguetteBox.js
-     *
-     * @since 1.10.0
-     *
-     * @param string  $value  The RegExp Pattern to match image files. Applied to the a.href attribute
-     */
+	 * Filters the image files filter of baguetteBox.js
+	 *
+	 * @since 1.10.0
+	 *
+	 * @param string  $value  The RegExp Pattern to match image files. Applied to the a.href attribute
+	 */
 	$baguettebox_filter = apply_filters( 'baguettebox_filter',  '/.+\.(gif|jpe?g|png|webp|svg|avif|heif|heic|tif?f|)($|\?)/i' );
 
 	wp_add_inline_script( 'baguettebox', 'window.addEventListener("load", function() {baguetteBox.run("' . $baguettebox_selector . '",{captions:function(t){var e=t.parentElement.classList.contains("wp-block-image")||t.parentElement.classList.contains("wp-block-media-text__media")?t.parentElement.querySelector("figcaption"):t.parentElement.parentElement.querySelector("figcaption,dd");return!!e&&e.innerHTML},filter:' . $baguettebox_filter . '});});' );
@@ -37,7 +37,27 @@ function register_assets() {
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\register_assets' );
 
 function enqueue_assets() {
-	if ( has_block( 'core/gallery' ) || has_block( 'core/image' ) || has_block( 'core/media-text' ) || get_post_gallery() || has_block('coblocks/gallery-masonry') || has_block('coblocks/gallery-stacked') || has_block('coblocks/gallery-collage') || has_block('coblocks/gallery-offset') || has_block('coblocks/gallery-stacked') ) {
+
+	/**
+	 * Filters whether baguettebox assets have to be enqueued.
+	 *
+	 * @since 1.10.2
+	 *
+	 * @param bool  $value  Whether baguettebox assets have to be enqueued.
+	 */
+	$baguettebox_enqueue_assets = apply_filters( 'baguettebox_enqueue_assets',
+		has_block( 'core/gallery' ) ||
+		has_block( 'core/image' ) ||
+		has_block( 'core/media-text' ) ||
+		get_post_gallery() ||
+		has_block( 'coblocks/gallery-masonry' ) ||
+		has_block( 'coblocks/gallery-stacked' ) ||
+		has_block( 'coblocks/gallery-collage' ) ||
+		has_block( 'coblocks/gallery-offset' ) ||
+		has_block( 'coblocks/gallery-stacked' )
+	);
+
+	if ( $baguettebox_enqueue_assets ) {
 		wp_enqueue_script( 'baguettebox' );
 		wp_enqueue_style( 'baguettebox-css' );
 	}
